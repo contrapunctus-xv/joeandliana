b4bbec8451f4a5f3c607641b8dea7eda57f2b11b
Joseph Lim <josephlim@Josephs-MBP.fios-router.home>
2017-02-08 23:11:08 -0500

    create heroku postbuild

--- server/server.js
+++ server/server.js
@@ -2,21 +2,34 @@ import express from 'express';
 import { devConfig as config} from '../webpack.config';
 import devMiddleware from 'webpack-dev-middleware';
 import hotMiddleware from 'webpack-hot-middleware';
+import { renderFile } from 'pug';
 import webpack from 'webpack';
 import path from 'path';
 
 const app = express();
 const port = 8080;
 const compiler = webpack(config);
-app.use(devMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}));
-app.use(hotMiddleware(compiler));
+let buildUrl;
 
+if (!process.env.NODE_ENV) {
+    app.use(devMiddleware(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }));
+    app.use(hotMiddleware(compiler));
+    buildUrl = config.output.publicPath;
 
-// set the home page route
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'../src/index.html')));
+}
+else if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    buildUrl = '';
+}
+
+const INDEX_PATH = path.resolve(__dirname,'../src/index.html.pug');
+app.get('/', (request, response) => {
+    const layout = renderFile(INDEX_PATH, {__BUILD_URL__: buildUrl});
+    response.send(layout);
+});
 
 app.listen(port, () =>
     console.log('Our app is running on http://localhost:' + port)
