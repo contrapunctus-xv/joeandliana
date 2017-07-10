3cb8d657d97cb6b252fb41397534aece93802b54
pipemadame <liana.chin@gmail.com>
2017-07-10 01:24:50 -0400

    resolves #15 registry info

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const FAQ = () =>
     <div className="container">
-        <h1>FAQ Page</h1>
+        <h1>FAQ</h1>
+        <div>
+            <img className="piggy-back" src="http://res.cloudinary.com/hfstexbmw/image/upload/v1499661478/joeandliana_DearStacey_Wedding_Photography_long_island0030_epq8d4.jpg"/>
+        </div>
+        <div className="text-body">
+            <h3>Where are we registered?</h3>
+            <ul>
+                <li>Check out our Zola registry <a href="https://www.zola.com/registry/lianaandjoseph">here</a></li>
+            </ul>
+            <h3>When is the wedding?</h3>
+            <ul>
+                <li>Saturday August 5, 2017 at 5:30pm</li>
+            </ul>
+            <h3>Where is the wedding?</h3>
+            <ul>
+                <li>Willow Creek Golf and Country Club on Long Island</li>
+                <li>Address: 1 Clubhouse Drive, Mt. Sinai, New York, 11766</li>
+            </ul>
+
+        </div>
+        <br/><br/>
         <div className="text-body"><a href="mailto:liana.and.joseph@gmail.com">Email Us</a> with questions at <strong>liana.and.joseph@gmail.com</strong></div>
-    </div>;
+    </div>
 
 export default FAQ;
